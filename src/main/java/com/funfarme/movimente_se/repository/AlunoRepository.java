fba3a2ed17fb46ff5defc2ca215fc447abe0d2c1
alisson-moura <alisson.mo.moura@outlook.com.br>
2026-03-26 14:40:17 -0300

    remove deps não utilizadas

--- src/main/java/com/funfarme/movimente_se/repository/AlunoRepository.java
+++ src/main/java/com/funfarme/movimente_se/repository/AlunoRepository.java
@@ -9,7 +9,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.*;
 import java.time.LocalDate;
